7a0906630de71655b8fffc76d1224af5e3d04ab3
Imran-2022 <mdimranulhaque202@gmail.com>
2024-03-10 19:53:52 +0600

    showing task in hm page completed

--- task_management/views.py
+++ task_management/views.py
@@ -1,4 +1,13 @@
+
 from django.shortcuts import render
+from task.models import Task
 
 def home(request):
-    return render(request,'home.html')
+    data = Task.objects.all()
+    # print(data)
+    # for i in data:
+        # print(i.title,i.category)
+        # for j in i.category.all():
+        #     print(j)
+    
+    return render(request,'home.html', {'data':data})
